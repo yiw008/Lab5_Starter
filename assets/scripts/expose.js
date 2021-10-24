431431b14390310da06e844f8f1c2c6d0bd7e6c5
Yizhou Wang <81718264+yiw008@users.noreply.github.com>
2021-10-23 17:17:21 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,50 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const jsConfetti = new JSConfetti();
+  var horn = document.getElementById("horn-select");
+  var images = document.getElementsByTagName("img");
+  var btn = document.getElementsByTagName("button");
+  var audio = document.getElementsByTagName("audio");
+  var volumeControl = document.getElementById("volume");
+
+  horn.addEventListener('input', function () {
+    if (horn.options[horn.selectedIndex].text == "Air Horn") {
+      images[0].src = "../assets/images/air-horn.svg";
+    } else if (horn.options[horn.selectedIndex].text == "Car Horn") {
+      images[0].src = "../assets/images/car-horn.svg";
+    } else if (horn.options[horn.selectedIndex].text == "Party Horn") {
+      images[0].src = "../assets/images/party-horn.svg";
+    } else {
+      images[0].src = "../assets/images/no-image.png";
+    }
+  })
+
+
+
+  volumeControl.addEventListener('click', function () {
+
+    if (volumeControl.value == 0) {
+      images[1].src = "../assets/icons/volume-level-0.svg";
+
+    } else if (volumeControl.value >= 1 && volumeControl.value < 33) {
+      images[1].src = "../assets/icons/volume-level-1.svg";
+    } else if (volumeControl.value >= 33 && volumeControl.value < 67) {
+      images[1].src = "../assets/icons/volume-level-2.svg";
+    } else { images[1].src = "../assets/icons/volume-level-3.svg"; }
+    audio[0].volume = volumeControl.value / 100;
+  })
+
+  btn[0].addEventListener('click', function () {
+    if (horn.options[horn.selectedIndex].text == "Air Horn") {
+      audio[0].src = "../assets/audio/air-horn.mp3";
+    } else if (horn.options[horn.selectedIndex].text == "Car Horn") {
+      audio[0].src = "../assets/audio/car-horn.mp3";
+    } else if (horn.options[horn.selectedIndex].text == "Party Horn") {
+      audio[0].src = "../assets/audio/party-horn.mp3";
+
+      jsConfetti.addConfetti();
+    }
+    audio[0].play();
+  })
 }
